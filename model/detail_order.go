fb6f208d8aac27c1364908e70475cbfe5659eca0
Saadyyyy <saadyyyy@LAPTOP-8CA1OA49>
2023-10-27 05:28:27 +0700

    Add foreign key DriveOrderAdmin

--- model/detail_order.go
+++ model/detail_order.go
@@ -6,10 +6,11 @@ import (
 
 type DetailOrder struct {
 	gorm.Model
-	MenuID        uint   `json:"event_id"` // ID acara yang makanannya terkait
-	UserID        uint   `json:"user_id"`  // ID pengguna yang membeli makanan
-	Quantity      int    `json:"quantity"` // Jumlah makanan yang dibeli
-	KodeVoucher   string `json:"kode_voucher"`
-	TotalCost     int    `json:"total_cost"`
-	InvoiceNumber string `json:"invoice_number"` // Nomor invoice untuk makanan
+	MenuID             uint   `json:"event_id"` // ID acara yang makanannya terkait
+	UserID             uint   `json:"user_id"`  // ID pengguna yang membeli makanan
+	Quantity           int    `json:"quantity"` // Jumlah makanan yang dibeli
+	KodeVoucher        string `json:"kode_voucher"`
+	TotalCost          int    `json:"total_cost"`
+	InvoiceNumber      string `json:"invoice_number"` // Nomor invoice untuk makanan
+	DriverOrderAdminID uint   // Add a foreign key to establish the relationship
 }
