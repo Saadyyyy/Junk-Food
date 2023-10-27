011951007e87d8ebf785890bb79720895b759cdb
Saadyyyy <saadyyyy@LAPTOP-8CA1OA49>
2023-10-28 06:27:15 +0700

    Change name function to GetVouchers

--- controller/get_voucher.go
+++ controller/get_voucher.go
@@ -9,7 +9,7 @@ import (
 	"gorm.io/gorm"
 )
 
-func GetPromos(db *gorm.DB, secretKey []byte) echo.HandlerFunc {
+func GetVouchers(db *gorm.DB, secretKey []byte) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		// Mendapatkan token dari header Authorization
 		tokenString := c.Request().Header.Get("Authorization")
