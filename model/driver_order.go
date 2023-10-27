3b37bee6403761b4ff0f6c9206425b26dbbb5213
Saadyyyy <saadyyyy@LAPTOP-8CA1OA49>
2023-10-27 07:14:37 +0700

    Fix bug struct driver order

--- model/driver_order.go
+++ model/driver_order.go
@@ -9,13 +9,13 @@ type DriverOrderAdmin struct {
 	Message          string
 	PickUpLocation   string        `json:"pick-up_location"`
 	DeliveryLocation string        `json:"delivery_location"`
-	DetailOrder      []DetailOrder `gorm:"foreignKey:DriverOrderAdminID"json:"detail_order"`
+	DetailOrder      []DetailOrder `gorm:"foreignKey:DriverOrderAdminID" json:"detail_order"`
 }
 
 type DriverOrderUser struct {
 	gorm.Model
-	UserID           uint   `json:"user_id"`
 	Message          string `json:"message"`
 	PickUpLocation   string `json:"pick-up_location"`
 	DeliveryLocation string `json:"delivery_location"`
+	UserID           uint   `json:"user_id"`
 }
