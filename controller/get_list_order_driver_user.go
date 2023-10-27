e8a166b9a6539753863ee6c1d5da347ceaebc5b4
Saadyyyy <saadyyyy@LAPTOP-8CA1OA49>
2023-10-27 07:13:14 +0700

    Add new fiture get all list order driver by user

---
+++ controller/get_list_order_driver_user.go
@@ -0,0 +1,50 @@
+package controller
+
+import (
+	"Junk-Food/middleware"
+	"Junk-Food/model"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"gorm.io/gorm"
+)
+
+func GetListOrderByUser(db *gorm.DB, secretKey []byte) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		// Mendapatkan token dari header Authorization
+		tokenString := c.Request().Header.Get("Authorization")
+		if tokenString == "" {
+			return c.JSON(http.StatusUnauthorized, map[string]interface{}{"error": true, "message": "Authorization token is missing"})
+		}
+
+		// Memeriksa apakah header Authorization mengandung token Bearer
+		if len(tokenString) < 7 || tokenString[:7] != "Bearer " {
+			return c.JSON(http.StatusUnauthorized, map[string]interface{}{"error": true, "message": "Invalid token format. Use 'Bearer [token]'"})
+		}
+
+		// Ekstrak token dari header
+		tokenString = tokenString[7:]
+
+		// Memverifikasi token
+		username, err := middleware.VerifyToken(tokenString, secretKey)
+		if err != nil {
+			return c.JSON(http.StatusUnauthorized, map[string]interface{}{"error": true, "message": "Invalid token"})
+		}
+
+		// Mengambil informasi user yang terkait dengan token
+		var user model.User
+		result := db.Where("username = ?", username).First(&user)
+		if result.Error != nil {
+			return c.JSON(http.StatusInternalServerError, map[string]interface{}{"error": true, "message": "Failed to fetch user data"})
+		}
+
+		// Mengambil daftar list dari database dengan detail orders
+		var orders []model.DriverOrderUser
+		if err := db.Find(&orders).Error; err != nil {
+			return c.JSON(http.StatusInternalServerError, map[string]interface{}{"error": true, "message": "Failed to fetch menu"})
+		}
+
+		// Mengembalikan daftar event dalam format yang diinginkan
+		return c.JSON(http.StatusOK, map[string]interface{}{"error": false, "events": orders})
+	}
+}
